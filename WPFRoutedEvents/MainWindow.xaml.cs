540b7fa0d03e27e75a39d0d1c26de4613a9cbcd7
Osazee Isere <oz1.concepts@gmail.com>
2021-12-26 20:16:29 +0100

    initial commit

--- WPFRoutedEvents/MainWindow.xaml.cs
+++ WPFRoutedEvents/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace WPFRoutedEvents
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _mouseActivity = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnClickMe_Clicked(object sender, RoutedEventArgs e)
+        {
+            AddEventInfo(sender, e);
+
+            MessageBox.Show(_mouseActivity,  "Your Event Info");
+            //clear string for next round.
+            _mouseActivity = "";
+
+
+        }
+
+        private void AddEventInfo(object sender, RoutedEventArgs e)
+        {
+            _mouseActivity += string.Format(
+                            "{0} sent a {1} event named {2}.\n", sender,
+                            e.RoutedEvent.RoutingStrategy,
+                            e.RoutedEvent.Name);
+        }
+
+        private void outerEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //// change title of the window
+            //this.Title = "You Clicked the outer ellipse";
+            //// stop bubbling
+            //e.Handled = true;
+            AddEventInfo(sender, e);
+        }
+
+        private void outerEllipse_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AddEventInfo(sender, e);
         }
     }
 }
